f097bac66e22bae653be813607ab7c9d20015ca0
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-21 23:53:37 +0000

    Add dev script

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import PhotoUploadButton from './components/PhotoUploadButton';
 
 function App() {
   return (
-    <PhotoUploadButton />
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <PhotoUploadButton />
+    </div>
   );
 }
 
